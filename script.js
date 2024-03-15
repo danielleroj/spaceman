a553270a8345ee479b40f16100702756b816c3ed
danielleroj <danielleroj@icloud.com>
2024-03-15 13:20:57 -0400

    adds audio

---
+++ script.js
@@ -0,0 +1,14 @@
+/*----- constants -----*/
+const AUDIO = new Audio('Morning-Routine-Lofi-Study-Music(chosic.com).mp3');
+
+/*----- app's state (variables) -----*/
+
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
