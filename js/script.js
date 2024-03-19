a83f55aa0a5962adfe6c486de15a69a2d2379867
danielleroj <danielleroj@icloud.com>
2024-03-19 00:44:47 -0400

    add keyboard functionality

--- js/script.js
+++ js/script.js
@@ -1,70 +1,81 @@
 /*----- constants -----*/
-const BACKGROUND_AUDIO = new Audio('Morning-Routine-Lofi-Study-Music(chosic.com).mp3');
-const WORDS = ['espresso', 'croissant', 'tiramisu'];
+const WORDS = ["espresso", "croissant", "tiramisu"];
 const MAX_GUESSES = 6;
-const SPACECAT_IMG = [
-    'imgs/image_part_000.PNG',
-    'imgs/image_part_001.png',
-    'imgs/image_part_002.png',
-    'imgs/image_part_003.png',
-    'imgs/image_part_004.png',
-    'imgs/image_part_005.png',
-    'imgs/image_part_006.png'
-];
+// const SPACECAT_IMG = [
+//   "imgs/image_part_000.PNG",
+//   "imgs/image_part_001.png",
+//   "imgs/image_part_002.png",
+//   "imgs/image_part_003.png",
+//   "imgs/image_part_004.png",
+//   "imgs/image_part_005.png",
+//   "imgs/image_part_006.png",
+// ];
+const word = "espresso";
 
 /*----- app's state (variables) -----*/
-let currentWord = 0;
-let incorrectGuesses = []; 
+let correctAnswer = " ";
+let currentWord = null;
+let guessedLetters = [];
+let wrongGuesses = 0;
 // let levels;
-let wordStatus = null;
 let gameStatus;
 
 // /*----- cached element references -----*/
-const messageEl = document.getElementById('message');
-const wordEl = document.getElementById('mystery-word');
-const playEl = document.getElementById('game-btn');
-const lettersEl = document.getElementById('letters');
-const spacecat = document.querySelector(".left-column img");
-// const levels = [
-//     {
-
-//     }];
+const messageEl = document.querySelector("#message");
+const wordEl = document.querySelector("#mystery-word");
+const playEl = document.querySelector("#game-btn");
+const letterBtns = document.querySelectorAll(".letters");
+const spacecat = document.querySelector("#left-img");
 
 /*----- event listeners -----*/
-game-btn.addEventListener('click', init);
-document.querySelector(.keyboard).addEventListener('click', handleClick);
-
+// game-btn.addEventListener('click', init);
 
 /*----- functions -----*/
-init()
+init();
 
-function handleClick() {
+function renderKeyboard() {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  letterBtns.forEach((button, idx) => {
+    const letter = alphabet[idx];
+    button.addEventListener("click", () => {
+      //   console.log(`${letter} was clicked.`);
+      guessedLetters.push(letter);
+      wordPlaceholder(word, guessedLetters);
+    });
+  });
+}
 
+function wordPlaceholder(word) {
+  const letters = word.split("");
+  const placeholder = "🧁";
+  let hideWord = letters.map((letter) => {
+    if (guessedLetters.includes(letter)) {
+      return letter;
+    } else {
+      return placeholder;
+    }
+  });
+  wordEl.innerText = hideWord.join("");
 }
 
 function init() {
-    currentWord = WORDS[0].split('');
-    incorrectGuesses = [];
-    // wordStatus = currentWord.map();
-    gameStatus = null;
-    render();
- }
-
- function renderGameStatus() {
-
- }
-    
-function renderMessage() {
-         if (gameStatus === 'W') {
-             messageEl.innerText = 'You did it! On to the next round';
-         } else if (gameStatus === 'L') {
-             messageEl.innerText = `Hmm, that's not right. The answer was ${wordEl}.`
-         }
-     }
-
-function render() {
-    wordEl.innerText = wordStatus.join("");
-    spacecat.src  = 'imgs/image_part_00${wrongGuesses.length}.png';
-    renderMessage;
+  //   wordStatus = currentWord.map();
+  //   gameStatus = null;
+  guessedLetters = [];
+  wordPlaceholder(word);
+  renderKeyboard();
+  render();
 }
 
+// function renderMessage() {
+//     if (gameStatus === 'W') {
+//         messageEl.innerText = 'You did it! On to the next round';
+//     } else if (gameStatus === 'L') {
+//         messageEl.innerText = `Hmm, that's not right. The answer was ${wordEl}.`
+//     }
+// }
+
+function render() {
+  spacecat.src = `imgs/image_part_0${wrongGuesses}.png`;
+  //   renderMessage;
+}
